3c9b49e155ab64b3f3401e6d846524d51c7e53b3
Ayush Nanda <ananda_be18@thapar.edu>
2020-11-17 18:42:15 +0530

    media dir + & urls modification

--- ZAPHUB/urls.py
+++ ZAPHUB/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include    #for apps we write here include
 
+#For media include these
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/' , include('shop.urls')),
     path('blog/' , include('blog.urls'))  #when someone comes to our website, they will see if shop exists. If yes then it will see include shops and will go tpo its, urls.py
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
