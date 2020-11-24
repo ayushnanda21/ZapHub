efdb855f846c0ff812655860ea7d353c14058979
Ayush Nanda <ananda_be18@thapar.edu>
2020-11-24 19:49:28 +0530

    category sorting

--- shop/views.py
+++ shop/views.py
@@ -1,11 +1,9 @@
-
-from django.shortcuts import render 
-from django.http import HttpResponse
-
-from .models import Product          #for fetching data from db
-from math import ceil        #for importing ceil
+from django.shortcuts import render
+from .models import Product
+from math import ceil
 
 # Create your views here.
+from django.http import HttpResponse
 
 def index(request):
     # products = Product.objects.all()
@@ -32,16 +30,16 @@ def about(request):
     return render(request, 'shop/about.html')
 
 def contact(request):
-    return HttpResponse("we are at contact")
+    return HttpResponse("We are at contact")
 
 def tracker(request):
-    return HttpResponse("we are at tracker")
+    return HttpResponse("We are at tracker")
 
 def search(request):
-    return HttpResponse("we are at search")
+    return HttpResponse("We are at search")
 
 def productview(request):
-    return HttpResponse("we are at productview")
+    return HttpResponse("We are at product view")
 
 def checkout(request):
-    return HttpResponse("we are at checkout")
+    return HttpResponse("We are at checkout")
